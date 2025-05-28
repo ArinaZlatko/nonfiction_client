5693ccf7a679f10819bba9519588f3d9fd6a74f0
ArinaZlatko <arinazlatko@gmail.com>
2025-05-28 12:22:33 +0300

    made null delete and edit comment

---
+++ src/app/books/comments/comment-delete/comment-delete.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comment-delete',
+  imports: [],
+  templateUrl: './comment-delete.component.html',
+  styleUrl: './comment-delete.component.css'
+})
+export class CommentDeleteComponent {
+
+}
